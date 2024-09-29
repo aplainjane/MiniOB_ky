d18d6a885451400065b711b355f0d421edfb0c40
aplainjane <1471336506@qq.com>
2024-09-29 11:25:39 +0800

    return

--- src/observer/sql/operator/update_physical_operator.cpp
+++ src/observer/sql/operator/update_physical_operator.cpp
@@ -21,7 +21,6 @@ using namespace std;
 std::vector<Record> insert_records;
 std::vector<Record> delete_records;
 
-
 UpdatePhysicalOperator::UpdatePhysicalOperator(Table *table, Field field, Value value)
     : table_(table), field_(field), value_(value)
 {}
@@ -30,12 +29,12 @@ RC UpdatePhysicalOperator::open(Trx *trx)
 {
   if (children_.empty()) {
     return RC::SUCCESS;
-  }  
+  }
 
   std::unique_ptr<PhysicalOperator> &child = children_[0];
-  RC rc = child->open(trx);
+  RC                                 rc    = child->open(trx);
 
-  if (rc != RC::SUCCESS){
+  if (rc != RC::SUCCESS) {
     LOG_WARN("failed to open child operator: %s", strrc(rc));
     return rc;
   }
@@ -54,73 +53,77 @@ RC UpdatePhysicalOperator::next()
 
   PhysicalOperator *child = children_[0].get();
 
-  // 用来临时存储要删除和插入的记录
-  std::vector<Record> delete_records;
   std::vector<Record> insert_records;
-
   while (RC::SUCCESS == (rc = child->next())) {
     Tuple *tuple = child->current_tuple();
     if (nullptr == tuple) {
       LOG_WARN("failed to get current record: %s", strrc(rc));
       return rc;
     }
-
     RowTuple *row_tuple = static_cast<RowTuple *>(tuple);
     Record   &record    = row_tuple->record();
 
-    // 将记录放入待删除列表
+    // rc = trx_->update_record(table_, record);
+    // 修改record
+    // rc = trx_->delete_record(table_, record);
     delete_records.emplace_back(record);
-
-    // 定位列名索引
-    const std::vector<FieldMeta> *table_field_metas = table_->table_meta().field_metas();
-    const char                   *target_field_name = field_.field_name();
-    int                           meta_num          = table_field_metas->size();
-    int                           target_index      = -1;
-
-    for (int i = 0; i < meta_num; ++i) {
-      const FieldMeta &fieldmeta = (*table_field_metas)[i];
-      if (0 == strcmp(fieldmeta.name(), target_field_name)) {
-        target_index = i;
-        break;
-      }
-    }
-
-    // 重新构造record
-    std::vector<Value> values;
-    int                cell_num = row_tuple->cell_num();
-
-    for (int i = 0; i < cell_num; ++i) {
-      Value cell;
-      if (target_index == i) {
-        cell.set_value(value_);
-      } else {
-        row_tuple->cell_at(i, cell);
-      }
-      values.emplace_back(cell);
-    }
-
-    Record new_record;
-    rc = table_->make_record(cell_num, values.data(), new_record);
-    if (rc != RC::SUCCESS) {
-      LOG_WARN("failed to make record. rc=%s", strrc(rc));
-      return rc;
-    }
-
-    insert_records.emplace_back(new_record);
-  }
-
-  // 先删除旧记录
-  for (const Record &record : delete_records) {
-    rc = trx_->delete_record(table_, record);
+    RC rc = RC::SUCCESS;
     if (rc != RC::SUCCESS) {
       LOG_WARN("failed to delete record: %s", strrc(rc));
       return rc;
+    } else {
+      // 定位列名索引
+      const std::vector<FieldMeta> *table_field_metas = table_->table_meta().field_metas();
+      const char                   *target_field_name = field_.field_name();
+
+      std::cout << "target field_name \t" << target_field_name << std::endl;
+
+      int meta_num     = table_field_metas->size();
+      int target_index = -1;
+      for (int i = 0; i < meta_num; ++i) {
+        FieldMeta fieldmeta = (*table_field_metas)[i];
+        // FieldMeta *fieldmeta = table_field_metas[i];
+        const char *field_name = fieldmeta.name();
+        if (0 == strcmp(field_name, target_field_name)) {
+          target_index = i;
+          break;
+        }
+      }
+      // 重新构造record
+      // 1. Values
+      std::vector<Value> values;
+      int                cell_num = row_tuple->cell_num();
+      for (int i = 0; i < cell_num; ++i) {
+        Value cell;
+        if (target_index == i) {
+          cell.set_value(value_);
+
+        } else {
+          row_tuple->cell_at(i, cell);
+        }
+        values.emplace_back(cell);
+      }
+      // 2. Record
+      Record new_record;
+      RC     rc = table_->make_record(cell_num, values.data(), new_record);
+      if (rc != RC::SUCCESS) {
+        LOG_WARN("failed to make record. rc=%s", strrc(rc));
+        return rc;
+      }
+      
+      insert_records.emplace_back(new_record);
     }
+
+    std::cout << "insert_record:" << insert_records.size() << std::endl;
   }
 
-  // 再插入新记录
-  for (const Record &record : insert_records) {
-    rc = trx_->insert_record(table_, record);
+  for (int i = 0; i < insert_records.size(); ++i) {
+    rc = trx_->delete_record(table_, delete_records[i]);
+    if (rc != RC::SUCCESS) {
+      LOG_WARN("failed to insert record: %s", strrc(rc));
+      return rc;
+    }
+    rc = trx_->insert_record(table_, insert_records[i]);
     if (rc != RC::SUCCESS) {
       LOG_WARN("failed to insert record: %s", strrc(rc));
       return rc;
@@ -130,10 +133,8 @@ RC UpdatePhysicalOperator::next()
   return RC::RECORD_EOF;
 }
 
-
 RC UpdatePhysicalOperator::close()
 {
-  
 
   if (!children_.empty()) {
     children_[0]->close();

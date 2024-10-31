8ffb1764e525521b2ab517ae61de256240cfe63b
dwjcb <1317385321@qq.com>
2024-10-31 20:51:52 +0800

    fix6

--- src/observer/sql/expr/expression.cpp
+++ src/observer/sql/expr/expression.cpp
@@ -297,11 +297,19 @@ RC ComparisonExpr::get_value(const Tuple &tuple, Value &value) const
   }
   else{
   RC rc = left_->get_value(tuple, left_value);
+  if(rc == RC::SQL_SYNTAX){
+    value.set_boolean(false);
+    return RC::SUCCESS;
+  }
   if (rc != RC::SUCCESS) {
     LOG_WARN("failed to get value of left expression. rc=%s", strrc(rc));
     return rc;
   }
   rc = right_->get_value(tuple, right_value);
+  if(rc == RC::SQL_SYNTAX){
+    value.set_boolean(false);
+    return RC::SUCCESS;
+  }
   if (rc != RC::SUCCESS) {
     LOG_WARN("failed to get value of right expression. rc=%s", strrc(rc));
     return rc;
@@ -466,6 +474,9 @@ RC ArithmeticExpr::calc_value(const Value &left_value, const Value &right_value,
     } break;
 
     case Type::DIV: {
+      if(right_value.get_int() == 0){
+        return RC::SQL_SYNTAX;
+      }
       Value::divide(left_value, right_value, value);
     } break;
 
